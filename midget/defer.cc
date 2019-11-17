dfb46b6dcbf34fc339627dd95da03dfa0c7e85a5
fenshitianyue <abzanda@163.com>
2019-11-17 10:27:36 +0800

    add command

--- midget/defer.cc
+++ midget/defer.cc
@@ -1,10 +1,16 @@
 #include <iostream>
-#include <utility>
+
+///////////////////////////////////////////////////////
+// 思路：
+//    1.通过填充lambda表达式的函数体来实现将任何语句封装成一个可调用对象
+//    2.将可调用对象作为模板类的成员，在析构函数中调用这个成员，这样就起到了在
+//      作用域结束后自动调用指定语句的作用
+///////////////////////////////////////////////////////
 
 template <typename T>
 class _Defer {
   public:
-    _Defer(T&& d) : _d(std::move(d)){}
+    _Defer(T&& d) : _d(std::move(d)){}  
     ~_Defer() { _d(); }
   private:
     T _d;
@@ -18,9 +24,8 @@ class Defer {
     }
 };
 
-#define PP_CONCAT_IMPL(perfix, suffix) prefix##suffix
-#define PP_CONCAT(prefix, suffix) PP_CONCAT_IMPL(prefix, suffix)
-#define UNIQUE_NAME(prefix) PP_CONCAT(prefix, __COUNTER__)
+#define CONCAT(perfix, suffix) prefix##suffix  // 拼接变量名
+#define UNIQUE_NAME(prefix) CONCAT(prefix, __COUNTER__)  // 保证变量名不会重复，让我们可以在同一个作用域调用多个defer
 #define DEFER auto UNIQUE_NAME(defer_) = Defer() + [&]
 
 void test() {
@@ -28,7 +33,6 @@ void test() {
 }
 
 int main(){
-  std::cout << "Enter main function->" << std::endl;
   std::cout << "Call test() function..." << std::endl;
   test();
   return 0;

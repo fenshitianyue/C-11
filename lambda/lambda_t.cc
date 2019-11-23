deb61dc5d14bf1eb1105a161c351cf3b4c266d14
fenshitianyue <abzanda@163.com>
2019-11-23 12:30:03 +0800

    update code

--- lambda/lambda_t.cc
+++ lambda/lambda_t.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <typeinfo>
 
 auto func_args = [](const std::string& s) {
   std::cout << s << std::endl;
@@ -12,5 +13,8 @@ auto func_non_args = [] {
 int main() {
   func_args("call func_args()..."); 
   func_non_args();
+  
+  std::cout << "func_args type is: "<< typeid(func_args).name() << std::endl;
+  std::cout << "func_non_args type is: " << typeid(func_non_args).name() << std::endl;
   return 0;
 }
